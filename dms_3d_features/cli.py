ef92810b9fd23521142806f63d2b8943ac9400f7
Joseph Yesselman <jyesselm@unl.edu>
2024-08-01 20:30:57 -0500

    got dssr torsions working

--- dms_3d_features/cli.py
+++ dms_3d_features/cli.py
@@ -1,9 +1,18 @@
 import click
+import glob
+import os
 
 from dms_3d_features.pdb_features import (
     compute_solvent_accessibility_all,
     calculate_hbond_strength,
+    calculate_structural_parameters_with_dssr,
+    DSSRTorsionFileProcessor,
+    get_all_torsional_parameters_from_dssr,
 )
+
+from dms_3d_features.logger import setup_logging, get_logger
+
+log = get_logger("cli")
 
 
 @click.group()
@@ -16,9 +25,13 @@ def get_pdb_features():
     """
     Get the solvent accessibility features for all PDB files in the pdbs directory.
     """
+    setup_logging()
     # df = compute_solvent_accessibility_all("data/pdbs")
     # df.to_csv("data/pdb-features/sasa.csv", index=False)
-    df = calculate_hbond_strength("data/pdbs")
+    # df = calculate_hbond_strength("data/pdbs")
+    # calculate_structural_parameters_with_dssr("data/pdbs")
+    df = get_all_torsional_parameters_from_dssr("data/pdbs")
+    df.to_csv("data/pdb-features/torsions.csv", index=False)
 
 
 if __name__ == "__main__":
--- dms_3d_features/pdb_features.py
+++ dms_3d_features/pdb_features.py
@@ -5,8 +5,14 @@ import freesasa
 import biopandas.pdb as PandasPdb
 import pandas as pd
 import os
-from typing import List
+from typing import List, Dict, Tuple, Union, Optional
 import subprocess
+import shutil
+
+
+from dms_3d_features.logger import get_logger
+
+log = get_logger("pdb-features")
 
 # solvent accessibility ##############################################################
 
@@ -295,3 +301,324 @@ def calculate_hbond_strength(pdb_dir) -> pd.DataFrame:
         break
 
     return pd.DataFrame(hbond_data)
+
+
+# dssr features #####################################################################
+
+
+def calculate_structural_parameters_with_dssr(pdb_dir: str) -> None:
+    """
+    Calculate structural parameters for PDB structures using DSSR.
+
+    This function processes all PDB files in the specified directory and
+    subdirectories. For each PDB file, it creates a corresponding directory,
+    moves the PDB file into this directory, and runs DSSR to calculate
+    structural parameters. The results are stored in output files named after
+    the original PDB file, with an additional extension '.out'. All output
+    files are moved to 'data/dssr-output'.
+
+    Args:
+        pdb_dir (str): The directory containing the PDB files.
+
+    Returns:
+        None
+    """
+    output_base_dir = os.path.join("data", "dssr-output")
+    os.makedirs(output_base_dir, exist_ok=True)
+    pdb_paths = glob.glob(f"{pdb_dir}/*/*.pdb")
+    log.info(f"Found {len(pdb_paths)} PDB files in {pdb_dir}")
+    for pdb_path in pdb_paths:
+        log.info(f"running dssr on: {pdb_path}")
+        pdb_filename = os.path.basename(pdb_path)
+        subprocess.call(
+            [
+                "x3dna-dssr",
+                f"-i={pdb_path}",
+                f"-o={output_base_dir}/{pdb_filename[:-4]}.out",
+            ],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        torsion_file_src = "dssr-torsions.txt"
+        torsion_file_dst = os.path.join(
+            output_base_dir, f"{pdb_filename[:-4]}_torsions.txt"
+        )
+        shutil.move(torsion_file_src, torsion_file_dst)
+        for temp_file in glob.glob(f"dssr-*"):
+            os.remove(temp_file)
+
+
+class DSSRTorsionFileProcessor:
+    """
+    A class to process DSSR torsion files and extract structural data.
+
+    Attributes:
+        output_dir (str): The directory to store the output files. Defaults to
+                          'dssr-output' in the given pdb_dir if not provided.
+    """
+
+    def __init__(self, output_dir: Optional[str] = None):
+        """
+        Initialize the DSSRTorsionFileProcessor with specified directories.
+        """
+        self.output_dir = (
+            output_dir if output_dir else os.path.join("data", "dssr-output")
+        )
+        os.makedirs(self.output_dir, exist_ok=True)
+
+    def process_file(
+        self, dssr_torsion_file: str, name: str, motif: str
+    ) -> pd.DataFrame:
+        """
+        Process a DSSR torsion file to extract structural data.
+
+        Args:
+            dssr_torsion_file (str): The path to the DSSR torsion file.
+            name (str): The name identifier for the data.
+            motif (str): The motif identifier for the data.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the extracted structural data.
+        """
+        lines = self.__read_file_lines(dssr_torsion_file)
+        start_indices = self.__find_start_indices(lines)
+
+        column_headers = self.__get_column_headers()
+        col_ranges = self.__get_column_ranges()
+
+        all_data = []
+        for i in range(len(motif) - 1):
+            row_data = self.__extract_row_data(lines, col_ranges, start_indices, i)
+            data = [name, motif] + row_data
+            all_data.append(data)
+
+        return pd.DataFrame(all_data, columns=column_headers)
+
+    # Helper functions ##############################################################
+    def __extract_substrings(
+        self, lines: List[str], indices: Dict[str, Tuple[int, int]], offset: int
+    ) -> List[str]:
+        """
+        Extract substrings from lines based on given indices.
+
+        Args:
+            lines (List[str]): The list of lines from the file.
+            indices (Dict[str, Tuple[int, int]]): A dictionary mapping keys to
+                tuples indicating the start and end indices for substring extraction.
+            offset (int): The offset line index for data extraction.
+
+        Returns:
+            List[str]: Extracted substrings from the specified line.
+        """
+        data = []
+        for _, (col_start, col_end) in indices.items():
+            if offset < len(lines):
+                line = lines[offset].strip()
+                data.append(line[col_start:col_end].strip())
+            else:
+                data.append("")  # Append an empty string if offset is out of range
+        return data
+
+    def __read_file_lines(self, file_path: str) -> List[str]:
+        """
+        Read all lines from a file.
+
+        Args:
+            file_path (str): The path to the file to be read.
+
+        Returns:
+            List[str]: A list of lines read from the file.
+        """
+        try:
+            with open(file_path, "r") as f:
+                return f.readlines()
+        except FileNotFoundError as e:
+            log.error(f"File not found: {file_path}")
+            return []
+
+    def __find_start_indices(self, lines: List[str]) -> Dict[str, int]:
+        """
+        Find the starting indices for various data sections in the lines.
+
+        Args:
+            lines (List[str]): The list of lines from the file.
+
+        Returns:
+            Dict[str, int]: A dictionary mapping section names to their start indices.
+        """
+        start_indices = {"alpha": -1, "eta": -1, "puckering": -1, "bin": -1}
+        for idx, line in enumerate(lines):
+            if "nt" in line:
+                if "alpha" in line:
+                    start_indices["alpha"] = idx + 1
+                elif "eta" in line:
+                    start_indices["eta"] = idx + 1
+                elif "v0" in line:
+                    start_indices["puckering"] = idx + 1
+                elif "bin" in line:
+                    start_indices["bin"] = idx + 1
+        if all(v == -1 for v in start_indices.values()):
+            log.warning("Not all data sections found in the file.")
+        return start_indices
+
+    def __get_column_headers(self) -> List[str]:
+        """
+        Get the column headers for the DataFrame.
+
+        Returns:
+            List[str]: A list of column headers.
+        """
+        return [
+            "pdb_name",
+            "m_sequence",
+            "r_loc_pos",
+            "r_nuc",
+            "alpha",
+            "beta",
+            "gamma",
+            "delta",
+            "epsilon",
+            "zeta",
+            "e-z",
+            "chi",
+            "phase-angle",
+            "sugar-type",
+            "sszp",
+            "dp",
+            "splay",
+            "eta",
+            "theta",
+            "eta_1",
+            "theta_1",
+            "eta_2",
+            "theta_2",
+            "v0",
+            "v1",
+            "v2",
+            "v3",
+            "v4",
+            "tm",
+            "p",
+            "puckering",
+            "bin",
+            "cluster",
+            "suitness",
+        ]
+
+    def __get_column_ranges(self) -> Dict[str, Dict[str, Tuple[int, int]]]:
+        """
+        Get the column ranges for data extraction.
+
+        Returns:
+            Dict[str, Dict[str, Tuple[int, int]]]: A dictionary mapping section
+            names to their corresponding column range mappings.
+        """
+        return {
+            "alpha": {
+                "nt": (8, 9),
+                "alpha": (26, 32),
+                "beta": (33, 40),
+                "gamma": (42, 48),
+                "delta": (51, 56),
+                "epsilon": (57, 65),
+                "zeta": (66, 73),
+                "e_z": (75, 83),
+                "chi": (85, 98),
+                "phase": (101, 116),
+                "sugar": (117, 127),
+                "ssZp": (130, 135),
+                "Dp": (138, 143),
+                "splay": (145, 151),
+            },
+            "eta": {
+                "eta": (25, 33),
+                "theta": (33, 41),
+                "eta_1": (41, 48),
+                "theta_1": (49, 56),
+                "eta_2": (57, 64),
+                "theta_2": (65, 73),
+            },
+            "puckering": {
+                "v0": (27, 33),
+                "v1": (34, 41),
+                "v2": (43, 49),
+                "v3": (50, 57),
+                "v4": (59, 64),
+                "tm": (67, 72),
+                "P": (75, 80),
+                "Puck": (81, 90),
+            },
+            "bin": {"bin": (24, 31), "clus": (33, 38), "suit": (42, 53)},
+        }
+
+    def __extract_row_data(
+        self,
+        lines: List[str],
+        col_ranges: Dict[str, Dict[str, Tuple[int, int]]],
+        start_indices: Dict[str, int],
+        offset: int,
+    ) -> List[str]:
+        """
+        Extract row data for a specific offset.
+
+        Args:
+            lines (List[str]): The list of lines from the file.
+            col_ranges (Dict[str, Dict[str, Tuple[int, int]]]): The column range mappings.
+            start_indices (Dict[str, int]): The starting indices for each section.
+            offset (int): The row offset for data extraction.
+
+        Returns:
+            List[str]: Extracted data for the specified row.
+        """
+        data_alpha = self.__extract_substrings(
+            lines, col_ranges["alpha"], start_indices["alpha"] + offset
+        )
+        data_eta = self.__extract_substrings(
+            lines, col_ranges["eta"], start_indices["eta"] + offset
+        )
+        data_puckering = self.__extract_substrings(
+            lines, col_ranges["puckering"], start_indices["puckering"] + offset
+        )
+        data_bin = self.__extract_substrings(
+            lines, col_ranges["bin"], start_indices["bin"] + offset
+        )
+        nt_num = self.__extract_substrings(
+            lines, {"nt_num": (9, 11)}, start_indices["alpha"] + offset
+        )
+        return nt_num + data_alpha + data_eta + data_puckering + data_bin
+
+
+def get_all_torsional_parameters_from_dssr(pdb_dir: str) -> pd.DataFrame:
+    """
+    Extract all torsional parameters from DSSR files in the given directory.
+
+    This function iterates through all PDB files in the specified directory and
+    processes the corresponding DSSR torsion files to extract structural parameters.
+
+    Args:
+        pdb_dir (str): The directory containing the PDB files.
+
+    Returns:
+        pd.DataFrame: A concatenated DataFrame containing torsional parameters
+                      for all processed PDB files.
+    """
+    pdb_paths = sorted(glob.glob(f"{pdb_dir}/*/*.pdb"))
+    dfs = []
+    for pdb_path in pdb_paths:
+        base_name = os.path.basename(pdb_path)[:-4]
+        motif = os.path.basename(os.path.dirname(pdb_path)).replace("_", "&")
+        log.info(f"Processing file: data/dssr-output/{base_name}_torsions.txt")
+
+        torsion_file_path = f"data/dssr-output/{base_name}_torsions.txt"
+        if os.path.exists(torsion_file_path):
+            processor = DSSRTorsionFileProcessor()
+            df = processor.process_file(torsion_file_path, base_name, motif)
+            dfs.append(df)
+        else:
+            log.warning(f"Torsion file not found: {torsion_file_path}")
+
+    if dfs:
+        return pd.concat(dfs, ignore_index=True)
+    else:
+        log.error("No data was processed. Check for missing files or errors.")
+        return pd.DataFrame()  # Return an empty DataFrame if no data was processed
